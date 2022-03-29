3a3f4432652efab0c0cbc0702ce6ff8c62ec23c1
iokim <iokim1220@gmail.com>
2022-03-29 10:14:35 +0900

    # 프록시와 내부 호출 - 문제 프록시 방식의 AOP는 메서드 내부 호출에 프록시를 적용할 수 없다

---
+++ src/main/java/hello/aop/internalcall/CallServiceVO.java
@@ -0,0 +1,19 @@
+package hello.aop.internalcall;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class CallServiceVO {
+
+    public void external() {
+        log.info("call external");
+        internal(); // 내부 메서드 호출(this.internal())
+    }
+
+    public void internal() {
+        log.info("call internal");
+    }
+
+}
---
+++ src/test/java/hello/aop/internalcall/CallServiceVOTest.java
@@ -0,0 +1,29 @@
+package hello.aop.internalcall;
+
+import hello.aop.internalcall.aop.CallLogAspect;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+@Import(CallLogAspect.class)
+@SpringBootTest
+class CallServiceVOTest {
+
+    @Autowired CallServiceVO callServiceVO;
+
+    @Test
+    void external() {
+        log.info("callServiceVO.getClass()={}", callServiceVO.getClass());
+        callServiceVO.external();
+    }
+
+    @Test
+    void internal() {
+        callServiceVO.internal();
+    }
+}
